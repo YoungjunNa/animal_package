0b6ec9ccda2900ab99c56e5a0fb36f383c57a74f
YoungjunNa <ruminoreticulum@gmail.com>
2017-11-03 10:35:44 +0900

    add equation for swine

---
+++ equation_swine.R
@@ -0,0 +1,13 @@
+##Equations for swine
+
+#Author: Youngjun Na
+#Email: ruminoreticulum@gmail.com
+#Github: https://github.com/YoungjunNa
+#Date: 11/3/2017
+
+#Digestible energy (DE)
+
+#Noblet and Perez, 1993
+DE <- function(GE, ASH, NDF){
+  1161+(0.749*GE)-(4.3*ASH)-(4.1*NDF)
+}
