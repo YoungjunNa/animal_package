629ac93f6b5f6f5a489702a8d859a212fde32326
YoungjunNa <ruminoreticulum@gmail.com>
2017-11-03 13:21:13 +0900

    add MEm requirement

--- hanwoo_nutrient_requirement.R
+++ hanwoo_nutrient_requirement.R
@@ -4,3 +4,11 @@
 #Email: ruminoreticulum@gmail.com
 #Github: https://github.com/YoungjunNa
 #Date: 11/3/2017
+
+#metabolizable energy for maintanance (MEm)
+
+MEm <- function(BW){
+  eq <- 0.125*(BW^(0.75))*(BW^(0.75))
+  print(eq)
+  print("Unit=kcal/day")
+}
