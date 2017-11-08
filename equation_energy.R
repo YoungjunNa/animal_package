711be15abf99f5e70d4b7f19f4e59db9e61eef94
YoungjunNa <ruminoreticulum@gmail.com>
2017-11-08 18:30:37 +0900

    add calory and joule translator

--- equation_energy.R
+++ equation_energy.R
@@ -5,6 +5,25 @@
 #Github: https://github.com/YoungjunNa
 #Date: 11/3/2017
 
+#calory to Joule
+cal_to_J <- function(cal){
+  eq <- cal*4.184
+  print(eq)
+  print("Joule")
+}
+
+cal_to_J(5000)
+  
+#Joule to calory
+
+J_to_cal <- function(J){
+  eq <- J*0.239006
+  print(eq)
+  print("cal")
+}
+
+J_to_cal(200)
+
 #Gross energy (GE; kcal/kg)
 
 #Ewan, 1989
@@ -13,3 +32,4 @@ GE <- function(EE, CP, ASH){
   print(eq)
   print("kcal/kg")
 }
+
