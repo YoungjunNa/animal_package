b64ac724fc9747e171c3125919d45c1463ad3074
YoungjunNa <ruminoreticulum@gmail.com>
2017-11-09 15:01:30 +0900

    add DE ME for swine

--- equation_energy.R
+++ equation_energy.R
@@ -15,21 +15,60 @@ cal_to_J <- function(cal){
 cal_to_J(5000)
   
 #Joule to calory
-
 J_to_cal <- function(J){
   eq <- J*0.239006
   print(eq)
-  print("cal")
+  print("Calory")
 }
 
 J_to_cal(200)
 
 #Gross energy (GE; kcal/kg)
-
 #Ewan, 1989
 GE <- function(EE, CP, ASH){
-  eq <- 4143+(56*EE)+(15*CP)-(44*ASH)
+  eq <- 4143+(5.6*EE)+(1.5*CP)-(4.4*ASH) #unit=g/kg DM
   print(eq)
   print("kcal/kg")
 }
+
+#Swine ===========================================================
+#Digestible energy (DE; kcal/kg)
+#Noblet and Perez, 1993
+DE_swine1 <- function(GE, ASH, NDF){
+  eq <- 1161+(0.749*GE)-(4.3*ASH)-(4.1*NDF) #unit=g/kg DM
+  print(eq)
+  print("kcal/kg")
+}
+
+DE_swine2 <- function(ASH, CP, EE, NDF){
+  eq <- 4168-(9.1*ASH)+(1.9*CP)+(3.9*EE)-(3.6*NDF) #unit=g/kg DM
+  print(eq)
+  print("kcal/kg")
+}
+
+#Metabolizable energy (ME; kcal/kg)
+#Noblet and Perez, 1993
+ME_swine1 <- function(ASH, CP, EE, NDF){
+  eq <- 4194-(9.2*ASH)+(1.0*CP)+(4.1*EE)-(3.5*NDF)
+  print(eq)
+  print("kcal/kg")
+}
+
+ME_swine2 <- function(DE, CP){
+  eq <- (1.0*DE)-(0.68*CP)
+  print(eq)
+  print("kcal/kg")
+}
+
+#calculator for GE, DE, ME
+GDME_swine <- function(ASH, CP, EE, NDF){
+  GE <- 4143+(5.6*EE)+(1.5*CP)-(4.4*ASH)
+  DE <- 4168-(9.1*ASH)+(1.9*CP)+(3.9*EE)-(3.6*NDF)
+  ME <- 4194-(9.2*ASH)+(1.0*CP)+(4.1*EE)-(3.5*NDF)
+  result <- data.frame("Item"=c("GE", "DE", "ME"), "Unit"="kcal/kg","Swine"=c(GE, DE, ME))
+  print(result)
+}
+
+
+
 
